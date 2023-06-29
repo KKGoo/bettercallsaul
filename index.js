011ad6567d7daf6b0fb3d2a7ac1fd53479e13d4b
Dealp08 <108693779+Dealp08@users.noreply.github.com>
2023-06-29 12:25:36 -0500

    Add list users

--- index.js
+++ index.js
@@ -1,89 +1,20 @@
-const usuarios = [
-    {
-        "nombre": "Juan",
-        "edad": 30,
-        "ciudad": "Madrid",
-        "casado": false,
-        "hobbies": ["fútbol", "leer", "bailar"]
-    },
-    {
-        "nombre": "María",
-        "edad": 25,
-        "ciudad": "Barcelona",
-        "casado": true,
-        "hobbies": ["pintar", "viajar", "cocinar"]
-    },
-    {
-        "nombre": "Carlos",
-        "edad": 40,
-        "ciudad": "Sevilla",
-        "casado": true,
-        "hobbies": ["música", "nadar", "jardinería"]
-    },
-    {
-        "nombre": "Ana",
-        "edad": 28,
-        "ciudad": "Valencia",
-        "casado": false,
-        "hobbies": ["yoga", "senderismo", "fotografía"]
-    },
-    {
-        "nombre": "Luis",
-        "edad": 35,
-        "ciudad": "Málaga",
-        "casado": true,
-        "hobbies": ["ciclismo", "pescar", "videojuegos"]
-    },
-    {
-        "nombre": "Laura",
-        "edad": 22,
-        "ciudad": "Bilbao",
-        "casado": false,
-        "hobbies": ["baile", "teatro", "compras"]
-    },
-    {
-        "nombre": "Pedro",
-        "edad": 31,
-        "ciudad": "Alicante",
-        "casado": false,
-        "hobbies": ["cocina", "pintura", "ajedrez"]
-    },
-    {
-        "nombre": "Sara",
-        "edad": 27,
-        "ciudad": "Granada",
-        "casado": true,
-        "hobbies": ["senderismo", "música", "ver películas"]
-    },
-    {
-        "nombre": "Miguel",
-        "edad": 33,
-        "ciudad": "Zaragoza",
-        "casado": true,
-        "hobbies": ["deportes", "lectura", "bricolaje"]
-    },
-    {
-        "nombre": "Isabel",
-        "edad": 29,
-        "ciudad": "Palma de Mallorca",
-        "casado": false,
-        "hobbies": ["surf", "pasear perros", "juegos de mesa"]
-    }
-];
-
 function users(data) {
     const table = document.getElementById('tabla');
-    const mapUsers = data.map((name) => `<tr>
-    <td>${name.nombre}</td><td>${name.edad}</td><td>${name.casado ? `si` : `no`}</td><td>${name.ciudad}</td><td>${name.hobbies } </td>
-    </tr>`)
+    const mapUsers = data.map((user) => `<tr>
+    <td>${user.name.first}</td><td>${user.dob.age}</td><td>${user.phone}</td><td>${user.location.city}</td></tr>`).join("")
     table.innerHTML = `
     <thead>
         <tr>
-            <th>Nombre</th><th>Edad</th><th>Casado</th><th>Ciudad</th><th>Hobbies</th>
+            <th>Nombre</th><th>Edad</th><th>Telefono</th><th>Ciudad</th>
         </tr>
     </thead>
     ${mapUsers}
 `;
 }
 
-users(usuarios)
+fetch("https://randomuser.me/api/?results=20")
+    .then(res => res.json())
+    .then((data) => {
+        console.log(data.results)
+        users(data.results)
+    })
